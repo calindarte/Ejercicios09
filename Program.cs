45af08151daa8ca1eb2df29a02b7d046db9ecfef
Carlos Santos <Carlos Santos@LAPTOP-H2K9SUQI>
2022-03-14 10:51:50 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe una frase");
+            string cadena = Console.ReadLine();
+
+            string cadenaMayusculas = cadena.ToUpper();
+
+            
+            Console.WriteLine(cadenaMayusculas);
+            Console.ReadLine(); 
+        }
+    }
+}
